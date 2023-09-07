71de0e8bf103c1080a407734008c04df9e68190b
Cristina Coelho <crissmcoelho@yahoo.com.br>
2023-09-06 21:38:23 -0300

    first commit

---
+++ livro/views.py
@@ -0,0 +1,27 @@
+from datetime import date, datetime
+from django.shortcuts import redirect, render
+from django.http import HttpResponse
+from usuarios.models import Usuario
+from . models import Livros, Categoria
+
+
+def home(request):
+    if request.session.get('usuario'):  
+        usuario = Usuario.objects.get(id = request.session['usuario'])
+        status_categoria = request.GET.get('cadastro_categoria')
+        livros = Livros.objects.filter(usuario = usuario)
+        total_livros = livros.count()
+        form = CadastroLivro()
+        form.fields['usuario'].initial = request.session['usuario']
+        form.fields['categoria'].queryset = Categoria.objects.filter(usuario = usuario)     
+        form_categoria = CategoriaLivro()
+        usuarios = Usuario.objects.all()
+        
+        
+def ver_livros(request, id):
+    if request.session.get('usuario'):
+        livro = Livros.objects.get(id = id)
+        if request.session.get('usuario') == livro.usuario.id:
+            usuario = Usuario.objects.get(id = request.session['usuario'])
+            categoria_livro = Categoria.objects.filter(usuario = request.session.get('usuario'))
+
